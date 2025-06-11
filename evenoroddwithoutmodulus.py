891833daac83173eb7371f9739b363747fb0a4ba
Prasanna Kuchipudi <prasannakuchipudi99@gmail.com>
2025-06-11 16:03:09 +0530

    Initial commit

---
+++ evenoroddwithoutmodulus.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a number:"))
+x=int(n/2)*2
+if(x==n):
+    print("Even number")
+else:
+    print("Odd number")
---
+++ sumofnumbersinarange.py
@@ -0,0 +1,6 @@
+start=int(input("Enter start value:"))
+end=int(input("Enter end value:"))
+n=0
+for i in range(start,end+1):
+    n+=i
+print(n)
