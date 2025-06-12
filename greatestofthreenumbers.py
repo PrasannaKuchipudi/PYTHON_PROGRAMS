d2acb4774f27535a803b406562b4bac596a281d6
Prasanna Kuchipudi <prasannakuchipudi99@gmail.com>
2025-06-12 23:08:12 +0530

    Initial commit

---
+++ greatestofthreenumbers.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+if a>b and a>c:
+    print("a is greatest value")
+elif b>a and b>c:
+    print("b is greatest value")
+else:
+    print("c is greatest value")
+
+
